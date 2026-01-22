91b3c481e5fc727e3f5d1a96b4171add89f10d81
wabung <progamermario42@gmail.com>
2026-01-22 08:43:52 +0100

    Added all new elements

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module aquaMotor.aquaMotorProyecto {
     requires javafx.controls;
     requires javafx.fxml;
 
+    opens aquaMotor.sales to javafx.fxml;
     opens aquaMotor.aquaMotorProyecto to javafx.fxml;
     exports aquaMotor.aquaMotorProyecto;
 }
