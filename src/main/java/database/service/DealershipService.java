95ca377fab0b70fe979d9e0860850b3f570f4b6d
jmolfer299 <jmolfer299@g.educaand.es>
2026-02-06 14:35:25 +0100

    I add the service

---
+++ src/main/java/database/service/DealershipService.java
@@ -0,0 +1,28 @@
+package database.service;
+
+import database.crud.*;
+
+public class DealershipService {
+
+    public final CrudBoss boss;
+    public final CrudClients clients;
+    public final CrudMechanic mechanic;
+    public final CrudOffer offer;
+    public final CrudParts parts;
+    public final CrudRepairment repairment;
+    public final CrudSalesPerson salesPerson;
+    public final CrudUser user;
+    public final CrudVehicle vehicle;
+
+    public DealershipService() {
+        this.boss = new CrudBoss();
+        this.clients = new CrudClients();
+        this.mechanic = new CrudMechanic();
+        this.offer = new CrudOffer();
+        this.parts = new CrudParts();
+        this.repairment = new CrudRepairment();
+        this.salesPerson = new CrudSalesPerson();
+        this.user = new CrudUser();
+        this.vehicle = new CrudVehicle();
+    }
+}
